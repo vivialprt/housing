95b26cd7dd685a0beb1b22685a427bba5925eb4c
Ivan Hryhoryeu <ivan.grigoryev502@gmail.com>
2025-10-14 21:32:23 +0300

    feat: add fetching of projects
    for each project first go to its page then fetch ads with additional request as they are loaded dynamically. After links extracted they are produced as others. Ad parser also extracts project info.

--- web_scraping/spiders/aruodas.py
+++ web_scraping/spiders/aruodas.py
@@ -1,4 +1,14 @@
+from enum import StrEnum
+from logging import getLogger
 import scrapy
+
+
+class PropertyPrefix(StrEnum):
+    FLAT_SALE = '1'
+    HOUSE_SALE = '2'
+    PREMISE_SALE = '3'
+    FLAT_RENT = '4'
+    HOUSE_RENT = '5'
 
 
 class AruodasSpider(scrapy.Spider):
@@ -9,7 +19,7 @@ class AruodasSpider(scrapy.Spider):
     def parse(self, r):
         
         standalone_links = r.css('.list-row-v2.object-row.selflat.advert  div.list-adress-v2 a::attr(href)').getall()
-        # project_links = r.css('.list-row-v2.object-row.selflat.project-chosen  div.list-adress-v2 a::attr(href)').getall()
+        project_links = r.css('.variants a::attr(href)').getall()
         next_page_bt = r.css('a.page-bt')[-1]
         next_page = r.follow(next_page_bt.css('::attr(href)').get())
         next_page_text = next_page_bt.css('::text').get()
@@ -17,11 +27,36 @@ class AruodasSpider(scrapy.Spider):
         for link in standalone_links:
             yield scrapy.Request(link, callback=self.parse_ad)
 
+        for link in project_links:
+            yield r.follow(link, callback=self.parse_project)
+
         if next_page_text == '»':
             yield next_page
 
+    def parse_project(self, r):
+        js_pattern = r'loadAdvertProjectAdverts\s*\((.*?)\);'
+        obj_type_id, advert_id, _, _, project_id, _, project_owner = r.css('script::text').re_first(js_pattern).split(', ')
+        params = {
+            'obj': obj_type_id,
+            'id': advert_id,
+            'project_id': project_id,
+            'project_owner': project_owner,
+        }
+        params_str = '&'.join(f'{k}={v}' for k, v in params.items())
+        yield r.follow(f'/ajax/getProjectAdverts/?{params_str}', callback=self.parse_project_variants)
+
+    def parse_project_variants(self, r):
+        for link in r.css('td.goto a::attr(href)').getall():
+            if link.startswith(f'/{PropertyPrefix.FLAT_SALE}'):
+                yield r.follow(link, callback=self.parse_ad)
+
     def parse_ad(self, r):
         city, district, street, obj_type = r.css('.obj-header-text::text').get().strip().split(', ')
+
+        project_name_raw = r.css('.project-in__title::text').get()
+        project_name = project_name_raw.strip()[1:-1] if project_name_raw else None
+        project_link = r.follow(r.css('a.project-in__button::attr(href)').get()).url if project_name_raw else None
+        project_developer_link = r.css('.projects-popup__developer__url a::attr(href)').get() if project_name_raw else None
 
         price_raw = r.css('span.price-eur::text').get().strip()
         price_per_m2_raw = r.css('span.price-per::text').get().strip()
@@ -48,6 +83,9 @@ class AruodasSpider(scrapy.Spider):
             'district': district,
             'street': street,
             'obj_type': obj_type,
+            'project_name': project_name,
+            'project_link': project_link,
+            'project_developer_link': project_developer_link,
             'price_raw': price_raw,
             'price_per_m2_raw': price_per_m2_raw,
             'precise_address_link': precise_address_link,
