467d91a9e8b5fffbc6a4f1f88e6c196e56aba832
Ivan Hryhoryeu <ivan.grigoryev502@gmail.com>
2025-11-06 21:49:15 +0200

    feat: add default headers
    idk, maybe it helps with 403.

--- web_scraping/settings.py
+++ web_scraping/settings.py
@@ -8,3 +8,14 @@ FEED_EXPORT_ENCODING = 'utf-8'
 USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/113.0'
 # COOCKIES_ENABLED = False
 # CLOSESPIDER_PAGECOUNT = 5
+
+DEFAULT_REQUEST_HEADERS = {
+    'Accept': '*/*',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Accept-Encoding': 'gzip, deflate, br, zstd',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'no-cors',
+    'Sec-Fetch-Site': 'same-origin'
+}
