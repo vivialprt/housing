88ab3fe0124e16b36aa1f6c007ea8f2eb284e32e
Ivan Hryhoryeu <ivan.grigoryev502@gmail.com>
2025-11-06 21:50:58 +0200

    chore: ruff format

--- web_scraping/settings.py
+++ web_scraping/settings.py
@@ -4,18 +4,18 @@ NEWSPIDER_MODULE = "web_scraping.spiders"
 ROBOTSTXT_OBEY = False
 CONCURRENT_REQUESTS = 1
 DOWNLOAD_DELAY = 2
-FEED_EXPORT_ENCODING = 'utf-8'
-USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/113.0'
+FEED_EXPORT_ENCODING = "utf-8"
+USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/113.0"
 # COOCKIES_ENABLED = False
 # CLOSESPIDER_PAGECOUNT = 5
 
 DEFAULT_REQUEST_HEADERS = {
-    'Accept': '*/*',
-    'Accept-Language': 'en-US,en;q=0.5',
-    'Accept-Encoding': 'gzip, deflate, br, zstd',
-    'Connection': 'keep-alive',
-    'Upgrade-Insecure-Requests': '1',
-    'Sec-Fetch-Dest': 'empty',
-    'Sec-Fetch-Mode': 'no-cors',
-    'Sec-Fetch-Site': 'same-origin'
+    "Accept": "*/*",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "no-cors",
+    "Sec-Fetch-Site": "same-origin",
 }
--- web_scraping/spiders/aruodas.py
+++ web_scraping/spiders/aruodas.py
@@ -3,25 +3,26 @@ import scrapy
 
 
 class PropertyPrefix(StrEnum):
-    FLAT_SALE = '1'
-    HOUSE_SALE = '2'
-    PREMISE_SALE = '3'
-    FLAT_RENT = '4'
-    HOUSE_RENT = '5'
+    FLAT_SALE = "1"
+    HOUSE_SALE = "2"
+    PREMISE_SALE = "3"
+    FLAT_RENT = "4"
+    HOUSE_RENT = "5"
 
 
 class AruodasSpider(scrapy.Spider):
     name = "aruodas"
-    allowed_domains = ['en.aruodas.lt']
-    start_urls = ['https://en.aruodas.lt/butai/vilniuje/']
+    allowed_domains = ["en.aruodas.lt"]
+    start_urls = ["https://en.aruodas.lt/butai/vilniuje/"]
 
     def parse(self, r):
-        
-        standalone_links = r.css('.list-row-v2.object-row.selflat.advert  div.list-adress-v2 a::attr(href)').getall()
-        project_links = r.css('.variants a::attr(href)').getall()
-        next_page_bt = r.css('a.page-bt')[-1]
-        next_page = r.follow(next_page_bt.css('::attr(href)').get())
-        next_page_text = next_page_bt.css('::text').get()
+        standalone_links = r.css(
+            ".list-row-v2.object-row.selflat.advert  div.list-adress-v2 a::attr(href)"
+        ).getall()
+        project_links = r.css(".variants a::attr(href)").getall()
+        next_page_bt = r.css("a.page-bt")[-1]
+        next_page = r.follow(next_page_bt.css("::attr(href)").get())
+        next_page_text = next_page_bt.css("::text").get()
 
         for link in standalone_links:
             yield scrapy.Request(link, callback=self.parse_ad)
@@ -29,72 +30,91 @@ class AruodasSpider(scrapy.Spider):
         for link in project_links:
             yield r.follow(link, callback=self.parse_project)
 
-        if next_page_text == '»':
+        if next_page_text == "»":
             yield next_page
 
     def parse_project(self, r):
-        js_pattern = r'loadAdvertProjectAdverts\s*\((.*?)\);'
-        obj_type_id, advert_id, _, _, project_id, _, project_owner = r.css('script::text').re_first(js_pattern).split(', ')
+        js_pattern = r"loadAdvertProjectAdverts\s*\((.*?)\);"
+        obj_type_id, advert_id, _, _, project_id, _, project_owner = (
+            r.css("script::text").re_first(js_pattern).split(", ")
+        )
         params = {
-            'obj': obj_type_id,
-            'id': advert_id,
-            'project_id': project_id,
-            'project_owner': project_owner,
+            "obj": obj_type_id,
+            "id": advert_id,
+            "project_id": project_id,
+            "project_owner": project_owner,
         }
-        params_str = '&'.join(f'{k}={v}' for k, v in params.items())
-        yield r.follow(f'/ajax/getProjectAdverts/?{params_str}', callback=self.parse_project_variants)
+        params_str = "&".join(f"{k}={v}" for k, v in params.items())
+        yield r.follow(
+            f"/ajax/getProjectAdverts/?{params_str}",
+            callback=self.parse_project_variants,
+        )
 
     def parse_project_variants(self, r):
-        for link in r.css('td.goto a::attr(href)').getall():
-            if link.startswith(f'/{PropertyPrefix.FLAT_SALE}'):
+        for link in r.css("td.goto a::attr(href)").getall():
+            if link.startswith(f"/{PropertyPrefix.FLAT_SALE}"):
                 yield r.follow(link, callback=self.parse_ad)
 
     def parse_ad(self, r):
-        city, district, street, obj_type = r.css('.obj-header-text::text').get().strip().split(', ')
+        city, district, street, obj_type = (
+            r.css(".obj-header-text::text").get().strip().split(", ")
+        )
 
-        sold = r.css('.adv-sold1-en').get() is not None
-        reserved = r.css('.reservation-strip').get() is not None
+        sold = r.css(".adv-sold1-en").get() is not None
+        reserved = r.css(".reservation-strip").get() is not None
 
-        project_name_raw = r.css('.project-in__title::text').get()
+        project_name_raw = r.css(".project-in__title::text").get()
         project_name = project_name_raw.strip()[1:-1] if project_name_raw else None
-        project_link = r.follow(r.css('a.project-in__button::attr(href)').get()).url if project_name_raw else None
-        project_developer_link = r.css('.projects-popup__developer__url a::attr(href)').get() if project_name_raw else None
+        project_link = (
+            r.follow(r.css("a.project-in__button::attr(href)").get()).url
+            if project_name_raw
+            else None
+        )
+        project_developer_link = (
+            r.css(".projects-popup__developer__url a::attr(href)").get()
+            if project_name_raw
+            else None
+        )
 
-        price_raw = r.css('span.price-eur::text').get().strip()
-        price_per_m2_raw = r.css('span.price-per::text').get().strip()
+        price_raw = r.css("span.price-eur::text").get().strip()
+        price_per_m2_raw = r.css("span.price-per::text").get().strip()
 
-        precise_address_link = r.css('a.link-obj-thumb.vector-thumb-map::attr(href)').get()
+        precise_address_link = r.css(
+            "a.link-obj-thumb.vector-thumb-map::attr(href)"
+        ).get()
 
-        details_dt = r.css('dl.obj-details dt::text').getall()
-        details_dd = [selector.css('::text').getall() for selector in r.css('dl.obj-details dd')]
+        details_dt = r.css("dl.obj-details dt::text").getall()
+        details_dd = [
+            selector.css("::text").getall() for selector in r.css("dl.obj-details dd")
+        ]
 
         details_raw = {
-            key.replace(':', '').strip(): value
+            key.replace(":", "").strip(): value
             for key, value in zip(details_dt, details_dd)
         }
 
-        decription = ''.join(r.css('.obj-comment div::text').getall())
+        decription = "".join(r.css(".obj-comment div::text").getall())
 
-        stats_dt = r.css('.obj-stats dt::text').getall()
-        stats_dd = r.css('.obj-stats dd::text').getall()
+        stats_dt = r.css(".obj-stats dt::text").getall()
+        stats_dd = r.css(".obj-stats dd::text").getall()
 
         stats_raw = {k: v for k, v in zip(stats_dt, stats_dd)}
 
         yield {
-            'city': city,
-            'district': district,
-            'street': street,
-            'obj_type': obj_type,
-            'sold': sold,
-            'reserved': reserved,
-            'project_name': project_name,
-            'project_link': project_link,
-            'project_developer_link': project_developer_link,
-            'price_raw': price_raw,
-            'price_per_m2_raw': price_per_m2_raw,
-            'precise_address_link': precise_address_link,
-            'details_raw': details_raw,
-            'decription': decription,
-            'stats_raw': stats_raw,
-            'url': r.url,
+            "city": city,
+            "district": district,
+            "street": street,
+            "obj_type": obj_type,
+            "sold": sold,
+            "reserved": reserved,
+            "project_name": project_name,
+            "project_link": project_link,
+            "project_developer_link": project_developer_link,
+            "price_raw": price_raw,
+            "price_per_m2_raw": price_per_m2_raw,
+            "precise_address_link": precise_address_link,
+            "details_raw": details_raw,
+            "decription": decription,
+            "stats_raw": stats_raw,
+            "url": r.url,
         }
