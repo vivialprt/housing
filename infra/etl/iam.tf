564f71141f3ea77cab8a09ac8be1f3aa396093b1
Ivan Hryhoryeu <ivan.grigoryev502@gmail.com>
2025-11-30 22:34:12 +0200

    feat: IAM role for ECS tasks

---
+++ infra/etl/iam.tf
@@ -0,0 +1,45 @@
+# resource "aws_iam_role" "eventbridge_role" {
+#   name = "eventbridge-role"
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [{
+#       Effect    = "Allow"
+#       Principal = { Service = "events.amazonaws.com" }
+#       Action    = "sts:AssumeRole"
+#     }]
+#   })
+# }
+
+resource "aws_iam_role" "ecs_task_role" {
+  name = "ecs-task-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "ecs-tasks.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "ecs_task_s3_policy" {
+    name = "ecs-task-s3-policy"
+    role = aws_iam_role.ecs_task_role.id
+    
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [{
+        Effect   = "Allow"
+        Action   = [
+            "s3:PutObject",
+            "s3:GetObject",
+            "s3:ListBucket"
+        ]
+        Resource = [
+            "${aws_s3_bucket.etl_raw_layer.arn}",
+            "${aws_s3_bucket.etl_raw_layer.arn}/*"
+        ]
+        }]
+    })
+}
