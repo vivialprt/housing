5242629f59dd178adca6bf4d127f5271cf21cd64
Ivan Hryhoryeu <ivan.grigoryev502@gmail.com>
2025-12-07 16:54:31 +0200

    feat: add env name in bucket name for more visibility

--- infra/etl/s3.tf
+++ infra/etl/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "etl_raw_layer" {
-  bucket   = "etl-raw-layer-${var.s3_postfix}"
+  bucket   = "${var.env}-etl-raw-layer-${var.s3_postfix}"
 
   tags = {
     "L2" = "ETL",
