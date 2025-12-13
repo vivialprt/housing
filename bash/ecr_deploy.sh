bc94909bc4a7093135ff2cab38079836d0c297bd
Ivan Hryhoryeu <ivan.grigoryev502@gmail.com>
2025-12-13 13:35:20 +0200

    feat: easier push
    setup AWS_PROFILE and REPO_URL, run build, run deploy

--- bash/ecr_deploy.sh
+++ bash/ecr_deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+export AWS_REGION=$(aws configure get region --profile $AWS_PROFILE)
+export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --profile $AWS_PROFILE --query Account --output text)
+export ECR_LOGIN_URL="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
+
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR_LOGIN_URL
 
 docker push $REPO_URL
--- bash/setup_aws.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-aws sso login --profile $AWS_PROFILE
-
-export AWS_REGION=$(aws configure get region --profile $AWS_PROFILE)
-export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --profile $AWS_PROFILE --query Account --output text)
-export ECR_LOGIN_URL="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
